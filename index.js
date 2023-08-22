1f45b27e3091b2e131b50afed57a02c560df6bd4
jacksakers <jsakers@email.sc.edu>
2023-08-22 13:24:54 -0400

    basic gui basically done

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 var testPrint = () => {
     console.log("TEST");
 }
+
+var submitNewBet = () => {
+  // var formData = new FormData(document.querySelector('form'))
+  var formData = {};
+  var title = document.getElementById("title").value;
+  // var betOptions = document.getElementById("title").value;
+  console.log(title);
+  return false;
+}
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   document.getElementById("new-bet-form").addEventListener("submit", function(e) {
+//       e.preventDefault() // Cancel the default action
+//       submitNewBet();
+//   });
+// });
